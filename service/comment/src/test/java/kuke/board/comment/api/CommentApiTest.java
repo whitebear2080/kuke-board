980978102261825edd75328fc4b1b86cdf4ade39
whitebear2080 <whitebear2080@naver.com>
2025-03-03 21:36:41 +0900

    댓글 최대 2 depth - 목록 조회 API 구현

--- service/comment/src/test/java/kuke/board/comment/api/CommentApiTest.java
+++ service/comment/src/test/java/kuke/board/comment/api/CommentApiTest.java
@@ -1,10 +1,14 @@
 package kuke.board.comment.api;
 
+import kuke.board.comment.service.response.CommentPageResponse;
 import kuke.board.comment.service.response.CommentResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 class CommentApiTest {
     RestClient restClient = RestClient.create("http://localhost:9001");
@@ -56,6 +60,73 @@ class CommentApiTest {
         //restClient.delete().uri("/v1/comments/{commentId}", 154790229113524224L).retrieve();
         //restClient.delete().uri("/v1/comments/{commentId}", 154790233454628864L).retrieve();
         restClient.delete().uri("/v1/comments/{commentId}", 154790233999888384L).retrieve();
+    }
+
+    @Test
+    void readAll() {
+        CommentPageResponse response = restClient.get()
+                .uri("/v1/comments?articleId=1&page=1&pageSize=10")
+                .retrieve()
+                .body(CommentPageResponse.class);
+
+        System.out.println("response.getCommentCount() = " + response.getCommentCount());
+        for (CommentResponse comment : response.getComments()) {
+            //2 depth 표시
+            if (!comment.getCommentId().equals(comment.getParentCommentId())) {
+                System.out.print("\t");
+            }
+            System.out.println("comment.getCommentId() = " + comment.getCommentId());
+        }
+
+        /**
+         * 1번 페이지 수행 결과
+         * response.getCommentCount() = 101
+         * comment.getCommentId() = 154804489481519104
+         * 	comment.getCommentId() = 154804489691234321
+         * comment.getCommentId() = 154804489481519105
+         * 	comment.getCommentId() = 154804489708011533
+         * comment.getCommentId() = 154804489481519106
+         * 	comment.getCommentId() = 154804489687040002
+         * comment.getCommentId() = 154804489481519107
+         * 	comment.getCommentId() = 154804489687040000
+         * comment.getCommentId() = 154804489481519108
+         * 	comment.getCommentId() = 154804489687040010
+         */
+    }
+
+    @Test
+    void readAllInfiniteScroll() {
+        List<CommentResponse> responses1 = restClient.get()
+                .uri("/v1/comments/infinite-scroll?articleId=1&pageSize=5")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<CommentResponse>>() {
+                });
+
+        System.out.println("firstPage");
+        for (CommentResponse comment : responses1) {
+            if (!comment.getCommentId().equals(comment.getParentCommentId())) {
+                System.out.print("\t");
+            }
+            System.out.println("comment.getCommentId() = " + comment.getCommentId());
+        }
+
+        Long lastParentCommentId = responses1.getLast().getParentCommentId();
+        Long lastCommentId = responses1.getLast().getCommentId();
+
+        List<CommentResponse> responses2 = restClient.get()
+                .uri("/v1/comments/infinite-scroll?articleId=1&pageSize=5&lastParentCommentId=%s&lastCommentId=%s"
+                        .formatted(lastParentCommentId, lastCommentId))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<CommentResponse>>() {
+                });
+
+        System.out.println("secondPage");
+        for (CommentResponse comment : responses2) {
+            if (!comment.getCommentId().equals(comment.getParentCommentId())) {
+                System.out.print("\t");
+            }
+            System.out.println("comment.getCommentId() = " + comment.getCommentId());
+        }
     }
 
     @Getter
