561623861c81246993d8cb7c3ec5a037c8fb0cb8
whitebear2080 <whitebear2080@naver.com>
2025-03-23 16:10:25 +0900

    조회수 구현 - 어뷰징 방지 구현
    . 10분에 2회이상 조회하는 것은 어뷰징으로 판단

--- service/view/src/main/java/kuke/board/view/ViewApplication.java
+++ service/view/src/main/java/kuke/board/view/ViewApplication.java
@@ -2,8 +2,12 @@ package kuke.board.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = "kuke.board")
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "kuke.board")
 public class ViewApplication {
 
     public static void main(String[] args) {
