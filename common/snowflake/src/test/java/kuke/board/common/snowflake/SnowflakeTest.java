36c3b4625c9b2ed98f01f1efd0a1592ce0279048
whitebear2080 <whitebear2080@naver.com>
2025-04-12 14:34:13 +0900

    Kafka 세팅, Event & 직렬화 모듈 구현

--- common/snowflake/src/test/java/kuke/board/common/snowflake/SnowflakeTest.java
+++ common/snowflake/src/test/java/kuke/board/common/snowflake/SnowflakeTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
